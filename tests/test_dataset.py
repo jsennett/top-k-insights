79086ae18bf30f72c0369d17b64789e5b8ab5d6d
Joshua Sennett <joshua.sennett@tufts.edu>
2020-04-08 11:34:43 -0400

    Added simple aggregate extractor

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -2,10 +2,18 @@ from top_k_insights.dataset import Dataset
 
 def test_dataset():
     cars_dataset = Dataset("/Users/jsennett/Code/top-k-insights/data/vehicle-sales.csv")
-    assert(cars_dataset.dimension_names == ["year", "brand", "country"])
-    assert(cars_dataset.measure_name == "vehicles")
+    assert(len(cars_dataset.dimensions) == 3)
+    assert(cars_dataset.measure == "vehicles")
     assert(len(cars_dataset.data) == 96)
-    assert(len(cars_dataset.data[0]) == 4)
+    assert(len(cars_dataset.data.columns) == 4)
 
+def test_extract():
+    cars_dataset = Dataset("/Users/jsennett/Code/top-k-insights/data/vehicle-sales.csv")
+    subspace = {"brand":"Toyota"}
+    dividing_dimension = "year"
+    extractor_type = "RANK"
 
-
+    results = cars_dataset.extract(subspace, dividing_dimension, extractor_type)
+    assert(len(results) != 0)
+    print(results)
+    assert(all(results.columns == ["year", "vehicles", "rank", "pct", "delta_avg", "delta_prev"]))
--- top_k_insights/dataset.py
+++ top_k_insights/dataset.py
@@ -1,15 +1,37 @@
-import csv
+import pandas as pd
+
+
 class Dataset:
     def __init__(self, filename):
         """
         Datasets should be csvs formatted with dimension columns to the left
         and measure as the final column. The first row should be a header.
         """
-        with open(filename, encoding='mac_roman', newline='') as csvfile:
-            reader = csv.reader(csvfile, delimiter=',')
-            first_row = next(reader)
-            self.dimension_names = first_row[:-1]
-            self.measure_name = first_row[-1]
-            self.data = []
-            for row in reader:
-                self.data.append(tuple(row))
+        self.data = pd.read_csv(filename, encoding='mac_roman')
+        self.measure = self.data.columns[-1]
+        self.dimensions = self.data.columns[:-1]
+
+
+    def extract(self, subspace, dividing_dimension, extractor):
+        """
+        subspace is a dict of ('dimension_name':'value') pairs
+            for filtered dimensions. Excluding a dimension means no filter.
+        extractor is one of ['RANK', 'DIFF_AVG', 'SUM', 'DIFF_PREV']
+        """
+        # Filter along all subspaces
+        subset = self.data.loc[(self.data[list(subspace)] == pd.Series(subspace)).all(axis=1)]
+
+        # Group by the dividing dimension
+        result_set = subset.groupby(dividing_dimension).\
+            agg({self.measure:'sum'})
+
+        # Calculate aggregate measures
+        result_set['rank'] = result_set.rank(ascending=False)
+        result_set['pct'] = 100 * result_set[self.measure] / result_set[self.measure].sum()
+        result_set['delta_avg'] = result_set[self.measure] - result_set[self.measure].sum() / len(result_set)
+        result_set = result_set.reset_index()
+        if "year" in result_set.columns:
+            result_set['delta_prev'] = result_set.sort_values("year")[self.measure].diff() / result_set.sort_values("year")["year"].diff()
+
+        return result_set
+
