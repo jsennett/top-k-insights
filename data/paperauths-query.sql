e61eabc4a2db495d48eea9416ecdf790c520aaa9
Joshua Sennett <joshua.sennett@tufts.edu>
2020-05-05 21:48:30 -0400

    Added requirements.txt

---
+++ data/paperauths-query.sql
@@ -0,0 +1,6 @@
+﻿-- paperauths
+select pa.paperid, pa.authid, v.year as year
+from paperauths pa, papers p, venue v
+WHERE pa.paperid = p.id
+AND p.venue = v.id
+AND v.year between 1990 AND 2015;
---
+++ data/papers-query.sql
@@ -0,0 +1,7 @@
+﻿-- papers
+select pa.paperid as paperid, v.name as venue_name, v.year as year, v.school as school, v.type as venue_type
+FROM papers p, venue v
+WHERE pa.paperid = p.id
+AND p.venue = v.id
+AND v.year between 1990 AND 2015
+LIMIT 10;
